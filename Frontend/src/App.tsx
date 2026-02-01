ee1d60366ae54a183c061e9d9bbbbf63809a773a
Gokul Js <gokuljs029@gmail.com>
2026-02-01 22:02:20 +0530

    Refactor App component to integrate video streaming functionality; remove unused CSS styles and enhance socket connection handling.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,20 +1,37 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
 import { io } from "socket.io-client";
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = "http://localhost:3000";
 function App() {
-  const [socket] = useState(io(BASE_URL))
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [socket] = useState(io(BASE_URL));
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log("connecting to socket server with id", socket.id)
-      console.log(socket)
-    })
-  }, [socket])
+    async function loadStream() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: true,
+        });
+        const video = videoRef.current;
+        if (video) {
+          video.srcObject = stream;
+          console.log("video stream connected successfully");
+        }
+      } catch (error) {
+        console.error("Error getting media", error);
+      }
+    }
+    socket.on("connect", () => {
+      console.log("connecting to socket server with id", socket.id);
+      console.log(socket);
+      loadStream();
+    });
+  }, [socket]);
   return (
-     <div>
-     <h1>Hello World</h1>
-     </div>
-  )
+    <div className="h-screen w-ful">
+      <video ref={videoRef} autoPlay playsInline />
+    </div>
+  );
 }
 
-export default App
+export default App;
